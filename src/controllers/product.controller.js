7a3ba68f1c5ec85b424547d8e08635bf290097cf
Rajdeep Singh <rajdeepsingh7733@gmail.com>
2024-10-29 13:15:41 +0530

    updating controller for adding new movie

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -5,5 +5,13 @@ class ProductController {
     const products = ProductModel.get()
     res.render('products', {products:products})
   }
+  getAddForm(req, res) {
+    return res.render('new-product')
+  }
+  addNewProduct(req, res) {
+    ProductModel.add(req.body)
+    const products = ProductModel.get()
+    return res.render('products', {products})
+  }
 }
 export default ProductController;
