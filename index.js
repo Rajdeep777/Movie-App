6bfb0d6dbf7f8d3735945c53018cfd3a85836c31
Rajdeep Singh <rajdeepsingh7733@gmail.com>
2024-10-30 12:30:14 +0530

    validating data in form using validation middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import ejsLayout from 'express-ejs-layouts'
 import ProductController from './src/controllers/product.controller.js'
+import validateRequest from './src/middlewares/validation.middleware.js'
 const app = express()
 const PORT = 8000
 // setup view engine
@@ -15,7 +16,7 @@ app.use(express.urlencoded({extended : true}))
 app.use(express.static('public'))
 app.get('/', productController.getProducts)
 app.get('/new', productController.getAddProduct)
-app.post('/', productController.postAddProduct)
+app.post('/', validateRequest, productController.postAddProduct)
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:8000');
 })
