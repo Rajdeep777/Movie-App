f6a16e02b249ce1e880680549c70a9118dc1b4b2
Rajdeep Singh <rajdeepsingh7733@gmail.com>
2024-11-05 11:04:16 +0530

    Deleting cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -29,11 +29,11 @@ class UserController {
     req.session.destroy((err) => {
       if (err) {
         console.log(err);
+      } else {
+        res.redirect("/login");
       }
-      else {
-        res.redirect('/login')
-      }
-    })
+    });
+    res.clearCookie("lastVisit");
   }
 }
 export default UserController;
